7a4b58d363a39b291b42d6493e4630409c28a07e
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2025-12-23 23:35:36 +0530

    basic screenshot functionality using playwright

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Capture Web
+
+Get high quality screenshots and pdf from website urls .
+
+<!-- ## Installation
+
+```bash
+npm install
+``` -->
+
+<!-- ## Usage
+
+```bash
+npm start
+```
+
+## Features
+
+- Feature 1
+- Feature 2
+- Feature 3 -->
+
+## Contributing
+
+Pull requests are welcome.
+
+<!-- ## License
+
+MIT -->
--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import {chromium} from 'playwright';
 const app=express();
-
+app.use(express.json());
 app.get('/',(req,res)=>res.send("server running"));
+app.post('/screenshot',async (req,res)=>{
+const url=req.body.url;
+const browser= await chromium.launch();
+const context = await browser.newContext({
+  viewport: { width: 1920, height: 1080 },
+  deviceScaleFactor: 2
+});
+const page = await context.newPage();
+await page.goto(`${url}`);
+await page.screenshot({ path: 'screenshot333.png' ,type: 'png'});
+  await browser.close();
+res.send({done:"job done"})
+
+})
 
 app.listen(3000,()=>console.log("listening on port 3000"));
